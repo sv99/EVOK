2e67ab24f0aabd167350c75b67d7418801ea76db
w7789475 <515944650@qq.com>
2018-06-23 15:57:52 +0800

    201806231557

--- evokXJ/evokNewXj/Properties/Settings.Designer.cs
+++ evokXJ/evokNewXj/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace evokNew.Properties {
+namespace evokNewXJ.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
