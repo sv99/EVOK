607986474082f1c87a1f509671c104437c4aee6d
w7789475 <515944650@qq.com>
2018-06-23 12:59:38 +0800

    添加台达程序

---
+++ evokNew/evokNew/WatchForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace evokNew
+{
+    public partial class WatchForm : Form
+    {
+        public WatchForm()
+        {
+            InitializeComponent();
+        }
+
+        public void SetShowDataTable(DataTable dt)
+        {
+            dgv.DataSource = dt;
+        }
+        private void watchForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
